4cb0c7244f0af02d28cf90d402a14ce45418c71c
Nikhil Tidke <nikhil.tidke@devrev.ai>
2025-04-18 19:45:55 +0530

    feat: enhance Moder component with framer-motion animations
    - Integrated framer-motion for improved animations in the Moder component, enhancing user experience.
    - Refactored the layout structure to utilize motion.div for smoother transitions and visual feedback.
    - Updated button elements to use motion for dynamic scaling and opacity effects during interactions.

--- libs/components/editor/_components/moder.tsx
+++ libs/components/editor/_components/moder.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useEffect } from "react";
 import { useEditor } from "../editor.context";
 import { ContentEditorMode } from "../modes/content-editor/content-editor";
@@ -15,24 +16,96 @@ export const Moder = () => {
     }, []);
 
     return (
-        <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 w-full max-w-2xl p-1 rounded-2xl bg-background/30 backdrop-blur-2xl border border-stone-300/50  saturate-200">
-            <div className="flex flex-col space-y-2 p-2 bg-background rounded-xl">
+        <motion.div
+            layout
+            className="mx-auto bg-accent p-1 rounded-2xl fixed bottom-8 left-1/2 -translate-x-1/2 max-w-[900px] max-h-fit"
+            style={{
+                transformOrigin: "bottom center",
+                willChange: "transform",
+            }}
+            animate={{
+                scale: 1,
+                filter: "blur(0px)",
+                originX: 0.5,
+                originY: 1,
+            }}
+        >
+            <motion.div
+                layout
+                initial={{
+                    scale: 0.9,
+                    opacity: 0,
+                    filter: "blur(5px)",
+                    originX: 0.5,
+                    originY: 1,
+                }}
+                animate={{
+                    scale: 1,
+                    opacity: 1,
+                    filter: "blur(0px)",
+                    originX: 0.5,
+                    originY: 1,
+                }}
+                exit={{
+                    scale: 0.9,
+                    opacity: 0,
+                    filter: "blur(5px)",
+                    originX: 0.5,
+                    originY: 1,
+                }}
+                transition={{
+                    duration: 0.2,
+                    ease: "easeOut",
+                    layout: {
+                        duration: 0.1,
+                        ease: "easeOut",
+                        originY: 1,
+                    },
+                    opacity: {
+                        value: 1,
+                        delay: 0.3,
+                        duration: 0.2,
+                        ease: "easeOut",
+                    },
+                }}
+                key={modeId}
+                style={{
+                    originY: 1,
+                    willChange: "transform",
+                    transformOrigin: "bottom center",
+                }}
+                className="bg-background rounded-lg overflow-hidden"
+            >
                 {Object.values(modes)
                     .find((mode) => mode.id === modeId)
                     ?.editorRenderer()}
-            </div>
-            <div className="flex gap-2">
+            </motion.div>
+            <motion.div className="w-full flex" layout>
                 {Object.values(modes).map((mode) => (
-                    <div
+                    <motion.button
                         key={mode.id}
-                        onClick={() => {
-                            setModeId(mode.id);
+                        className="p-2"
+                        onClick={() => setModeId(mode.id)}
+                        layout
+                        initial={{
+                            scale: 0.9,
+                            opacity: 0,
+                            filter: "blur(5px)",
+                        }}
+                        animate={{
+                            scale: 1,
+                            opacity: 1,
+                            filter: "blur(0px)",
+                        }}
+                        transition={{
+                            duration: 0.1,
+                            ease: "easeOut",
                         }}
                     >
                         {mode.actionRenderer?.(mode.id === modeId)}
-                    </div>
+                    </motion.button>
                 ))}
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     );
 };
