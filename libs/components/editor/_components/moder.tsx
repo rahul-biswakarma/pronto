d1506fc533631a8ff389eea6a12a3ba2304e9862
Rahul Biswakarma <rahul.id39@gmail.com>
2025-04-18 18:34:48 +0530

    refactor: streamline mode rendering in Moder component
    - Updated the Moder component to render only the active editor mode based on modeId, improving performance and clarity.
    - Simplified the rendering logic by removing the mapping of all modes, focusing on the selected mode's editorRenderer.

--- libs/components/editor/_components/moder.tsx
+++ libs/components/editor/_components/moder.tsx
@@ -15,9 +15,9 @@ export const Moder = () => {
     return (
         <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 w-full max-w-2xl p-1 rounded-2xl bg-background/30 backdrop-blur-2xl border border-stone-300/50  saturate-200">
             <div className="flex flex-col space-y-2 p-2 bg-background rounded-xl">
-                {Object.values(modes).map((mode) => (
-                    <div key={mode.id}>{mode.editorRenderer()}</div>
-                ))}
+                {Object.values(modes)
+                    .find((mode) => mode.id === modeId)
+                    ?.editorRenderer()}
             </div>
             <div className="flex gap-2">
                 {Object.values(modes).map((mode) => (
