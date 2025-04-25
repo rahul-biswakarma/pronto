da0bb01cd2f2cc49266c38f6a5f6cf56e3e17b93
Rahul Biswakarma <rahul.id39@gmail.com>
2025-04-25 18:42:54 +0530

    refactor: enhance portfolio editor functionality and improve routing
    - Updated the PortfolioEditorPage to streamline portfolio editing with improved authentication checks and data handling.
    - Refined the API route for creating portfolio routes, ensuring robust error handling and CSRF protection.
    - Enhanced user navigation with the OWXPage component for better portfolio selection.
    - Improved context management with EditorProvider and RouteProvider for effective state handling.
    - Optimized AddRouteInput component with better error handling and loading states for user feedback.
    - Updated routing constants for improved navigation consistency across the application.

---
+++ app/onx/page.tsx
@@ -0,0 +1,26 @@
+import { PortfolioSelector } from "@/libs/components/selector/portfolio-selector";
+import { ROUTES } from "@/libs/constants/routes";
+import { checkAuthentication } from "@/libs/utils/auth";
+import { redirect } from "next/navigation";
+
+const ONXPage = async () => {
+    const { authenticated, errorResponse, supabase, userId } =
+        await checkAuthentication();
+
+    if (!authenticated || errorResponse) {
+        redirect("/login");
+    }
+
+    const { data: portfolioData, error: portfolioError } = await supabase
+        .from("portfolio")
+        .select("domain, id")
+        .eq("user_id", userId);
+
+    if (portfolioError || !portfolioData) {
+        redirect(ROUTES.Onboarding);
+    }
+
+    return <PortfolioSelector portfolioData={portfolioData} />;
+};
+
+export default ONXPage;
